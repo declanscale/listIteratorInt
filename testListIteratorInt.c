a10622791beb6e66249c8bee4a721f388ceae5bd
declan <declanscale@gmail.com>
2017-12-28 10:56:50 +0800

    Create testListIteratorInt.c

---
+++ testListIteratorInt.c
@@ -0,0 +1,66 @@
+/*
+  client to test listIteratorInt.
+  Written by ....
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include "listIteratorInt.h"
+
+//void show(IteratorInt IT);
+
+
+int main(int argc, char *argv[])
+{
+/*
+
+    IteratorInt it;
+    it = IteratorIntNew();
+
+    int i = 0;
+    int ifAdd = 9;
+
+    while(i<3){
+
+        ifAdd = add(it,i);
+        printf("%d\n",ifAdd);
+        show(it);
+
+        i++;
+    }
+
+
+
+//------------------------------------------------ test previous
+
+    printf("\n");
+    printf("----------------------------- test previous()\n");
+
+    int *value1 = previous(it);
+    printf("%d\n",*value1);
+    show(it);
+
+    printf("\n");
+
+    int *value2 = previous(it);
+    printf("%d\n",*value2);
+//    show(it);
+
+    int *value3 = previous(it);
+    printf("%d\n",*value3);
+//    show(it);
+
+
+
+//-------------------------------------------------
+
+*/
+
+
+  return EXIT_SUCCESS;
+
+}
+
+
